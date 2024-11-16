7ac9df1bb208f68c7129e9310c721538bc9c48bf
dnxa <kuridze0@gmail.com>
2024-11-16 12:50:06 +0400

    Add corsheaders middleware to intallet apps and middlewares

--- l17/l17/settings.py
+++ l17/l17/settings.py
@@ -30,8 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'chat_page',
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
